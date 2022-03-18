2f2de4bb2971ea6275aa74d4900fafc03f851bc6
rhokanson <99600920+rhokanson@users.noreply.github.com>
2022-03-18 09:47:08 +0900

    img delete

--- js/backgroud.js
+++ js/backgroud.js
@@ -1,5 +1,5 @@
 const images = [
-    "0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg", "8.jpeg", "9.jpeg", "10.jpeg", "11.jpeg", "12.jpeg"
+    "0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg", "8.jpeg", "9.jpeg", "10.jpeg"
 ];
 
 
