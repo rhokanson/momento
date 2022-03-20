537cbbc9132f2df2abdabd0d52d374cf84869e70
rhokanson <99600920+rhokanson@users.noreply.github.com>
2022-03-20 21:46:10 +0900

    진짜 최종

--- js/greetings.js
+++ js/greetings.js
@@ -4,12 +4,30 @@ const loginGreeting = document.querySelector("#login-form span");
 const greeting = document.querySelector("#greeting");
 
 const clock2 = document.querySelector("h2#clock");
-const date2 = document.querySelector("#date-text");
+const dateText2 = document.querySelector("#date-text");
 
 const editNameBtn = document.querySelector("h4")
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
+
+const date = new Date();
+const hours = date.getHours();
+let chosenGreeting = "";
+const random = Math.random();
+if (random < 0.1) {
+    chosenGreeting = "Don't forget to drink water";
+} else if (random > 0.9){
+    chosenGreeting = "A Reminder to move your body";
+} else if(hours < 5) {
+    chosenGreeting = "Get some sleep for tomorrow";
+} else if (hours < 12) {
+chosenGreeting = "Good morning";
+} else if(hours < 17) {
+chosenGreeting = "Good afternoon";
+} else if(hours < 24) {
+chosenGreeting = "Good evening";
+}
 
 function onLoginSubmit(event) {
     event.preventDefault();
@@ -22,24 +40,10 @@ function onLoginSubmit(event) {
 
 function paintGreetings(){
     const username = localStorage.getItem(USERNAME_KEY);
-    const date = new Date();
-    const hours = date.getHours();
-    let chosenGreeting = "";
-    if (Math.random() < 0.1) {
-        chosenGreeting = "Don't forget to drink water";
-    } else if(hours < 5) {
-        chosenGreeting = "Get some sleep for tomorrow";
-    } else if (hours < 12) {
-    chosenGreeting = "Good morning";
-    } else if(hours < 17) {
-    chosenGreeting = "Good afternoon";
-    } else if(hours < 24) {
-    chosenGreeting = "Good evening";
-    }
     greeting.classList.remove(HIDDEN_CLASSNAME);
     greeting.innerText =`${chosenGreeting}, ${username}`;
     clock2.classList.remove(HIDDEN_CLASSNAME);
-    date2.classList.remove(HIDDEN_CLASSNAME);
+    dateText2.classList.remove(HIDDEN_CLASSNAME);
     editNameBtn.classList.remove(HIDDEN_CLASSNAME);
 }
 
