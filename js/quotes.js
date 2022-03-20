1105f9379b943ea9e81f7335ec8d064372398830
rhokanson <99600920+rhokanson@users.noreply.github.com>
2022-03-20 23:29:49 +0900

    진짜진짜 최종

--- js/quotes.js
+++ js/quotes.js
@@ -8,7 +8,7 @@ const quotes = [
         author: "Benjamin Franklin",
     },
     {
-        quote: '"Spread love everywhere you go. Let no one ever come to you without leaving happier."',
+        quote: '"Spread love everywhere you go.\nLet no one ever come to you without leaving happier."',
         author: "Mother Teresa",
     },
     {
@@ -24,15 +24,30 @@ const quotes = [
         author: "William Shakespeare",
     },
     {
-        quote: '"Love, free as air at sight of human ties, Spreads his light wings, and in a moment flies."',
+        quote: '"Love, free as air at sight of human ties,\nSpreads his light wings, and in a moment flies."',
         author: "Alexander Pope",
     },
 ]
 
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
+const quoteDiv = document.querySelector("#quote")
 
 const todaysQuote = (quotes[Math.floor(Math.random() * quotes.length)]);
 
 quote.innerText = todaysQuote.quote;
 author.innerText = todaysQuote.author;
+
+
+function handleResize() {  
+    const quoteWidth = quoteDiv.offsetWidth; 
+    if (quoteWidth + 300 > window.innerWidth) {
+    quoteDiv.classList.add("translateY")
+    }
+    else {
+    quoteDiv.classList.remove("translateY")
+    }
+}
+
+handleResize()
+window.addEventListener("resize", handleResize)
